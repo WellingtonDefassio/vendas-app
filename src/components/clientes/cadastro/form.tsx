c3eeaea7ddfaf8e70bc74f5f5bf2e153f6a6f2a5
Wellington Defassio <well.defassio@gmail.com>
2022-09-08 00:26:23 -0300

    commit

--- src/components/clientes/cadastro/form.tsx
+++ src/components/clientes/cadastro/form.tsx
@@ -1,12 +1,13 @@
 import { Cliente } from "app/models/clientes";
 import { useFormik } from "formik";
+import { Input } from "components";
 
 interface ClienteFormProps {
   cliente: Cliente;
   onSubmit: (cliente: Cliente) => void;
 }
 
-const formScheme: Cliente = {
+const formSchema: Cliente = {
   cadastro: "",
   cpf: "",
   dataNascimento: "",
@@ -22,19 +23,104 @@ export const ClienteForm: React.FC<ClienteFormProps> = ({
   onSubmit,
 }) => {
   const formik = useFormik<Cliente>({
-    initialValues: { ...cliente, ...formScheme },
+    initialValues: { ...formSchema, ...cliente },
     onSubmit,
   });
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <input
-        value={formik.values.nome}
-        onChange={formik.handleChange}
-        id="nome"
-        name="nome"
-      />
-      <button type="submit">ENVIAR</button>
+      {formik.values.id && (
+        <div className="columns">
+          <Input
+            label="Código: "
+            id="id"
+            name="id"
+            autoComplete="off"
+            disabled={true}
+            value={formik.values.id}
+            typeDiv="input"
+          />
+          <Input
+            label="Data de Cadastro: "
+            id="cadastro"
+            name="cadastro"
+            autoComplete="off"
+            disabled={true}
+            value={formik.values.cadastro}
+            typeDiv="input"
+          />
+        </div>
+      )}
+
+      <div className="columns">
+        <Input
+          label="Nome: *"
+          id="nome"
+          name="nome"
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.nome}
+          typeDiv="input"
+        />
+      </div>
+      <div className="columns">
+        <Input
+          label="CPF: *"
+          id="cpf"
+          name="cpf"
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.cpf}
+          typeDiv="input"
+        />
+        <Input
+          label="Data Nascimento: *"
+          id="dataNascimento"
+          name="dataNascimento"
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.dataNascimento}
+          typeDiv="input"
+        />
+      </div>
+      <div className="columns">
+        <Input
+          label="Endereço: *"
+          id="endereco"
+          name="endereco"
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.endereco}
+          typeDiv="input"
+        />
+      </div>
+      <div className="columns">
+        <Input
+          label="Email: *"
+          id="email"
+          name="email"
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          typeDiv="input"
+        />
+        <Input
+          label="Telefone: *"
+          id="telefone"
+          name="telefone"
+          autoComplete="off"
+          onChange={formik.handleChange}
+          value={formik.values.telefone}
+          typeDiv="input"
+        />
+      </div>
+      <div className="field is-grouped">
+        <div className="control is-link">
+          <button type="submit" className="button">
+            {formik.values.id ? "Atualizar" : "Salvar"}
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
